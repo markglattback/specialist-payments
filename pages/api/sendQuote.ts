ec744f15597cb27e96c1c4571ca5f6ff4303e59f
Mark Glattback <mark.glattback@gmail.com>
2020-09-24 22:12:22 +0100

    yet more tests

--- pages/api/sendQuote.ts
+++ pages/api/sendQuote.ts
@@ -68,24 +68,14 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     .slice(0, 1)
     .toUpperCase()}${body.lastName.slice(1)}`;
 
-  res.statusCode = 200;
-  res.json({ user: process.env.SMTP_USER, pass: process.env.SMTP_PASS });
-  try {
-    await transporter
-      .verify()
-      .then(() => {
-        /* do nothing */
-      })
-      .catch((err) => {
-        throw Error(err.message);
-      });
+  console.log(transporter);
 
-    const message = {
-      from: process.env.SMTP_USER,
-      to: process.env.SMTP_USER,
-      replyTo: body.email,
-      subject: `Web Enquiry - ${sender}`,
-      text: `First Name: ${body.firstName},
+  const message = {
+    from: process.env.SMTP_USER,
+    to: process.env.SMTP_USER,
+    replyTo: body.email,
+    subject: `Web Enquiry - ${sender}`,
+    text: `First Name: ${body.firstName},
       Last Name: ${body.lastName},
       Email: ${body.email},
       Phone: ${body.phone},
@@ -96,50 +86,54 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       Payment Types: ${paymentTypes},
       Comments: ${body.comments},
       `,
-      html: `<h1 style="font-size: 1.25em">Web Enquiry</h1>
+    html: `<h1 style="font-size: 1.25em">Web Enquiry</h1>
       <p><span style="font-weight: bold;">First Name:</span> ${validator.escape(
         body.firstName
       )}</p>
-      <p><span style="font-weight: bold;">Last Name:</span> ${validator.escape(
-        body.lastName
-      )}</p>
-      <p><span style="font-weight: bold;">Email:</span> ${validator.escape(
-        body.email
-      )}</p>
-      <p><span style="font-weight: bold;">Phone:</span> ${validator.escape(
-        body.phone
-      )}</p>
-      <p><span style="font-weight: bold;">Business Name:</span> ${validator.escape(
-        body.business
-      )}</p>
-      <p><span style="font-weight: bold;">Customer Type:</span> ${validator.escape(
-        body.customerType
-      )}</p>
-      <p><span style="font-weight: bold;">Business Type:</span> ${validator.escape(
-        body.businessType
-      )}</p>
-      <p><span style="font-weight: bold;">Turnover:</span> ${validator.escape(
-        body.turnover
-      )}</p>
-      <p><span style="font-weight: bold;">Payment Types:</span> ${paymentTypes}</p>
-      <p><span style="font-weight: bold;">Comments:</span> ${validator.escape(
-        body.comments
-      )}</p>
-      `,
-    };
+        <p><span style="font-weight: bold;">Last Name:</span> ${validator.escape(
+          body.lastName
+        )}</p>
+          <p><span style="font-weight: bold;">Email:</span> ${validator.escape(
+            body.email
+          )}</p>
+            <p><span style="font-weight: bold;">Phone:</span> ${validator.escape(
+              body.phone
+            )}</p>
+              <p><span style="font-weight: bold;">Business Name:</span> ${validator.escape(
+                body.business
+              )}</p>
+                <p><span style="font-weight: bold;">Customer Type:</span> ${validator.escape(
+                  body.customerType
+                )}</p>
+                  <p><span style="font-weight: bold;">Business Type:</span> ${validator.escape(
+                    body.businessType
+                  )}</p>
+                    <p><span style="font-weight: bold;">Turnover:</span> ${validator.escape(
+                      body.turnover
+                    )}</p>
+                      <p><span style="font-weight: bold;">Payment Types:</span> ${paymentTypes}</p>
+                      <p><span style="font-weight: bold;">Comments:</span> ${validator.escape(
+                        body.comments
+                      )}</p>
+                        `,
+  };
 
-    await transporter
-      .sendMail(message)
-      .then(() => {
-        res.statusCode = 200;
-        res.json({ message: "Success" });
-      })
-      .catch((err) => {
-        throw Error(err.message);
+  transporter.verify(function (err, success) {
+    if (err) {
+      console.log(err);
+      res.statusCode = 500;
+      res.json({ message: "Server Error 1" });
+    } else {
+      transporter.sendMail(message, function (err, info) {
+        if (err) {
+          console.log(err);
+          res.statusCode = 500;
+          res.json({ message: "Server Error 1" });
+        } else {
+          res.statusCode = 200;
+          res.json({ message: "Server Error 2" });
+        }
       });
-  } catch (err) {
-    console.error(err);
-    res.statusCode = 500;
-    res.json({ message: err, env: process.env });
-  }
+    }
+  });
 };
